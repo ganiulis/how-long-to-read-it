352da369f6dfeaf0c54fd32a4f2de96c0bf4668c
adamappsdev <adamappsdev@gmail.com>
2020-08-13 10:32:17 +0300

    Updates

---
+++ precache-manifest.abcf664344a0d8268f2d98fd06e069b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e3cd15d8e17795556dd085bb448fd73",
+    "url": "/how-long-to-read-it/index.html"
+  },
+  {
+    "revision": "c1a3517cd3c46cfe283b",
+    "url": "/how-long-to-read-it/static/css/main.a91790a4.chunk.css"
+  },
+  {
+    "revision": "f8e979d16be29e3615fd",
+    "url": "/how-long-to-read-it/static/js/2.f0aa8577.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/how-long-to-read-it/static/js/2.f0aa8577.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1a3517cd3c46cfe283b",
+    "url": "/how-long-to-read-it/static/js/main.e4947518.chunk.js"
+  },
+  {
+    "revision": "13e5c7343c1ef0698bdd",
+    "url": "/how-long-to-read-it/static/js/runtime-main.2ab41a6d.js"
+  },
+  {
+    "revision": "bcadcf1e6931226674767bb3254d1275",
+    "url": "/how-long-to-read-it/static/media/background.bcadcf1e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/how-long-to-read-it-/precache-manifest.ea0d88198a77dfce26d1fc26992165b1.js"
+  "/how-long-to-read-it/precache-manifest.abcf664344a0d8268f2d98fd06e069b3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/how-long-to-read-it-/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/how-long-to-read-it/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
